df316d821f9916146ace355d80b26ff383cee5fe
Anna <103360175+Smileshum07@users.noreply.github.com>
2023-11-12 20:09:36 +0000

    start quiz

---
+++ assets/js/logic.js
@@ -0,0 +1,100 @@
+// Quiz questions and answers
+var quizQuestions = [
+    {
+        question: '1. How many hearts does an octopus have?',
+        options: [
+            '5 hearts',
+            '4 hearts',
+            '3 hearts',
+            '2 hearts',
+            '1 heart'
+        ],
+        answer: '3 hearts',
+    },
+    {
+        question: '2. What is acrophobia a fear of?',
+        options: [
+            'Heights',
+            'Widths',
+            'Spiders',
+            'Acrobats',
+            'Ants'
+        ],
+        answer: 'Heights',
+    },
+    {
+        question: '3. Which is the only body part that is fully grown from birth?',
+        options: [
+            'Hands',
+            'Legs',
+            'Heart',
+            'Head',
+            'Eyes'
+        ],
+        answer: 'Eyes',
+    },
+    {
+        question: '4. What planet is closest to the sun?',
+        options: [
+            'Earth',
+            'Mars',
+            'Mercury',
+            'Jupiter',
+            'Venus'
+        ],
+        answer: 'Mercury',
+    },
+    {
+        question: '5. What is the only continent with land in all four hemispheres?',
+        options: [
+            'North America',
+            'Africa',
+            'Australia',
+            'Antarctica',
+            'Eurasia'
+        ],
+        answer: 'Africa',
+    },
+];
+   
+    
+//console.log(quizQuestions);
+//console.log(Object.values(quizQuestions[0])[1]);
+
+// Select DOM elements
+var quizButton = document.getElementById('start');
+var startScreen = document.getElementById('start-screen');
+var question = document.getElementById('question-title');
+var choices = document.getElementById('choices');
+var time = document.getElementById('time');
+var secondsLeft = 75;
+
+// Function that starts quiz
+function startQuiz () {
+    startScreen.remove();
+    questions.setAttribute('class', 'show');
+    question.textContent = Object.values(quizQuestions[0])[0];
+       
+   
+    // timer
+    var timerInterval = setInterval(function() {
+    secondsLeft--;
+    time.textContent = secondsLeft;
+
+    if(secondsLeft === 0) {
+    // Stops execution of action at set interval
+      clearInterval(timerInterval);
+    };
+
+  }, 1000);
+   
+    for ( var i = 0; i < Object.values(quizQuestions[0])[1].length; i++ ) {
+        var answer = document.createElement('button');
+        answer.textContent = Object.values(quizQuestions[0][1][i]);
+        choices.appendChild(answer);
+    };
+    
+};
+      
+quizButton.addEventListener('click', startQuiz);
+
